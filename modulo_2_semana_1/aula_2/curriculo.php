fd6af7dfb3ed65ded37e133fa45362315125e7ba
fnascimentosilva <fabricionsilva26@gmail.com>
2023-09-30 12:35:45 +0100

    criando um novo arquivo curriculo

---
+++ modulo_2_semana_1/aula_2/curriculo.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Fabrício do Nascimento da Silva";
+$age = 36;
+$salary_expectation = 3500.00;
+$description = "Me chamo Fabrício, sou formado em análise e desenvolvimento de sistemas e estou fazendo um curso de Desenvolvedor FullStack pela DevinHouse!!!";
+$open_to_negotiation = true;
+?>
