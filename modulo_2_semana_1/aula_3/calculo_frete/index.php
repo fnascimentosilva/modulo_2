37ffb1d19bd1cc249bb1504ca2f347172a66e301
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-08 17:31:10 +0100

    inserindo calculadora de frete

--- modulo_2_semana_1/aula_3/calculo_frete/index.php
+++ modulo_2_semana_1/aula_3/calculo_frete/index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculo Frete</title>
+
+    <style>
+        .container {
+            margin: 0 auto;
+            width: 40%;
+            border: 1px solid #000;
+            padding: 10px;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
 </head>
+
 <body>
+    <?php echo $_SERVER['PHP_SELF'] ?>
+
+    <form class="container" method="post" action="resultado.php">
+    
+        <label>Peso do produto:</label>
+        <input type="number" name="peso">
+        <br>
+        <label>Distancia entrega:</label>
+        <input type="number" name="distancia">
+        <br>
+
+        <button type="submit">Calcular Frete</button>
+    </form>
+
     
 </body>
+
 </html>
--- modulo_2_semana_1/aula_3/calculo_frete/resultado.php
+++ modulo_2_semana_1/aula_3/calculo_frete/resultado.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cartas Férias</title>
+
+    <style>
+        .container {
+            margin: 0 auto;
+            width: 40%;
+            border: 1px solid #000;
+            padding: 10px;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
 </head>
+
+
 <body>
-    
+<?php
+    if (isset($_POST['peso']) && isset($_POST['distancia'])) {
+        $peso = $_POST['peso']*0.8;
+        $distancia = $_POST['distancia']*0.2;
+        $total_frete = $peso + $distancia;
+
+    ?>
+
+        <div class="container">
+            <p>Valor Total do frete: <?php echo $total_frete ?> R$</p>
+            
+        </div>
+    <?php
+    } else {
+        echo "Aguardando o preenchimento das informações";
+    }
+    ?>
 </body>
 </html>
