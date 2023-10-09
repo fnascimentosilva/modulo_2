4f858985aa1deef6cb23a621f7ceed31a85dd914
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-09 11:39:21 +0100

    iniciando arquivo para envio de arquivo database

--- modulo_2_semana_1/aula_3/variaveis_super_globais.php
+++ modulo_2_semana_1/aula_3/variaveis_super_globais.php
@@ -3,6 +3,9 @@
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 if($method === 'GET') {
@@ -17,6 +20,12 @@ if($method === 'GET') {
 }else if($method === 'POST') {
     $body = file_get_contents("php://input");
     $data = json_decode($body);
+    var_dump($data);
+
+    //ler arquivo para jogar dentro do database.txt
+
+    $text = file_get_contents('database.txt');
+    $arrayItens = json_decode($text);
 
     
     echo json_encode(['mensagem'=> 'recebi um POST' ]);
