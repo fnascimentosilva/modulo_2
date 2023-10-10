74acc36a51e175d8e91cf714e71c8ec7d740cae0
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-10 01:51:33 +0100

    concluindo uma requisicao de emprestimo estudantil

--- modulo_2_semana_1/aula_4/emprestimo_estudantil/emprestimo_estudantil.php
+++ modulo_2_semana_1/aula_4/emprestimo_estudantil/emprestimo_estudantil.php
@@ -1,5 +1,6 @@
 <?php 
 
+define('TAXA', 0.015);
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
@@ -14,7 +15,34 @@ if($method === 'POST'){
     $body = file_get_contents("php://input");
     $data = json_decode($body);
 
-    echo json_encode($data);
+
+    $nome = filter_var($data -> nome, FILTER_SANITIZE_SPECIAL_CHARS);
+    $idade = filter_var($data -> idade, FILTER_VALIDATE_INT);
+    $curso = filter_var($data -> curso, FILTER_SANITIZE_SPECIAL_CHARS);
+    $valor = filter_var($data -> valor, FILTER_VALIDATE_FLOAT);
+    $prazo = filter_var($data -> prazo, FILTER_VALIDATE_INT);
+
+    if(!$nome || !$idade || !$curso || !$valor || !$prazo){
+        echo json_encode(['error' => 'campo é obrigatorio']);
+    }
+
+    $juros = $valor * TAXA * $prazo;
+
+    $montante = $valor + $juros;
+
+    $parcela = $montante/$prazo;
+
+    echo json_encode([
+        'nome' => $nome,
+        'curso' => $curso,
+        'valor_curso' => $valor,
+        'prazo_pagamento' => $prazo,
+        'juros' => $juros,
+        'parcela' => number_format($parcela,2),
+        'valor_total' => $montante
+    ]);
+
+   
 }
 
 
