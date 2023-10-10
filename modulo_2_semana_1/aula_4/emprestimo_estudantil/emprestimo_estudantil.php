e2bcccff2ef6768f77068d3a50e363b6a98b9328
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-10 14:02:39 +0100

    inserindo um aruiqvo txt apos as validacoes

--- modulo_2_semana_1/aula_4/emprestimo_estudantil/emprestimo_estudantil.php
+++ modulo_2_semana_1/aula_4/emprestimo_estudantil/emprestimo_estudantil.php
@@ -12,25 +12,65 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $method = $_SERVER['REQUEST_METHOD'];
 
 if($method === 'POST'){
+    //recebe o body em formato de string
     $body = file_get_contents("php://input");
+    //converte para um array php
     $data = json_decode($body);
 
-
+//faz um filtro em todos as variaveis impedindo de vir alguma informacao diderente do declarado
     $nome = filter_var($data -> nome, FILTER_SANITIZE_SPECIAL_CHARS);
     $idade = filter_var($data -> idade, FILTER_VALIDATE_INT);
     $curso = filter_var($data -> curso, FILTER_SANITIZE_SPECIAL_CHARS);
     $valor = filter_var($data -> valor, FILTER_VALIDATE_FLOAT);
     $prazo = filter_var($data -> prazo, FILTER_VALIDATE_INT);
-
+//depois dos dados tratados tera uma condicao pra saber se nenhum campo virá vazio
     if(!$nome || !$idade || !$curso || !$valor || !$prazo){
         echo json_encode(['error' => 'campo é obrigatorio']);
+        exit;
     }
+
+    if($idade < 18){
+       http_response_code(403);
+        echo json_encode(['message' => 'Não permitido emprestimo a um menor!']);
+    }else if($idade < 25){
+        $juros = $valor * (1/100) * $prazo;
+
+    $montante = $valor + $juros;
+
+    $parcela = $montante/$prazo;
+
+    if(file_exists("$nome.txt")){
+        http_response_code(409);
+        echo json_encode(['error' => 'Ja existe um emprestimo em seu nome!']);
+    }else{
+    file_put_contents("$nome.txt", "Nome: $nome \n Idade: $idade \n Curso: $curso \n  ");
+
+    
+    echo json_encode([
+        'nome' => $nome,
+        'curso' => $curso,
+        'valor_curso' => $valor,
+        'prazo_pagamento' => $prazo,
+        'juros' => $juros,
+        'parcela' => number_format($parcela,2),
+        'valor_total' => $montante
+    ]);
+}
+    }else{
 
     $juros = $valor * TAXA * $prazo;
 
     $montante = $valor + $juros;
 
     $parcela = $montante/$prazo;
+
+
+    //inserindo um arquivo txt
+    if(file_exists("$nome.txt")){
+        http_response_code(409);
+        echo json_encode(['error' => 'Ja existe um emprestimo em seu nome!']);
+    }else{
+    file_put_contents("$nome.txt", "Nome: $nome \n Idade: $idade \n Curso: $curso \n  ");
 
     echo json_encode([
         'nome' => $nome,
@@ -41,9 +81,10 @@ if($method === 'POST'){
         'parcela' => number_format($parcela,2),
         'valor_total' => $montante
     ]);
-
-   
 }
-
-
-?>
+}
+   
+}else{
+    http_response_code(404);
+    echo ['message' => 'Essa operação náo é permitida!'];
+}
