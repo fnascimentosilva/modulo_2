9d936f93df8f9586f1ff4fa230a05cc4cba7440a
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-15 21:57:27 +0100

    exemplos de filter e map

--- modulo_2_semana_2/aula_1/index.php
+++ modulo_2_semana_2/aula_1/index.php
@@ -5,8 +5,48 @@ $products = [];
 
 var_dump($products);
 
-array_push($products, ['cafe', 'pao']);
+array_push($products, 'cafe', 'pao', 'papel');
 
 var_dump($products);
+
+
+//array filter
+$produtosFiltrados = array_filter($products, function($item){
+return $item === 'papel';
+});
+
+
+echo '<br>';
+echo '<br>';
+
+echo '</pre>';
+
+var_dump($produtosFiltrados);
+
+//array map
+
+$peoples = [
+[
+    'name' => 'Fabricio',
+    'sobrenome' => 'Silva'
+],
+
+[
+    'name' => 'Rosicleia',
+    'sobrenome' => 'Almeida'
+]
+];
+
+$novoArray = array_map(function($item){
+    return [...$item, 'fullname' => $item['name']. ' '. $item['sobrenome']];
+}, $peoples);
+
+echo '<br>';
+echo '<br>';
+echo '</pre>';
+
+var_dump($novoArray);
+
+
 
 ?>
