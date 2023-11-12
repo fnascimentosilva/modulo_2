a58c4920abad45edabe203a4409f142bfef2ccb4
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-12 21:24:05 +0000

    criando uma classe

---
+++ modulo_2_semana_3/aula_2/Pessoa.php
@@ -0,0 +1,15 @@
+<?php
+//1-Criar a classe
+//2-Criar os atributos
+//3-Criar os metodos
+
+class Pessoa
+{
+    public $nome;
+    public $idade;
+    public $cpf;
+
+    public function pegarCpf(){
+        
+    }
+}
