7f7b91735b2e803ac83dcf734fbc6eb72334d463
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-12 21:25:47 +0000

    usando uma funcao de data

---
+++ modulo_2_semana_3/aula_1/index.php
@@ -0,0 +1,10 @@
+<?php 
+echo 'to aqui de novo';
+
+date_default_timezone_set('Europe/Lisbon');//para ajustar a data e hora ao local que estou
+$agora = new DateTime();
+echo "<pre>";
+var_dump($agora);
+echo "<pre>";
+
+?>
