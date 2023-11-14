ded075268840fdc5397abb58f05d0f267f237591
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-14 12:09:47 +0000

    ajustando funcao pra receber id

--- modulo_2_semana_3/aula_2/models/Empresa.php
+++ modulo_2_semana_3/aula_2/models/Empresa.php
@@ -5,26 +5,61 @@ require_once 'Funcionario.php';
 //metodos(funcoes)
 
 class Empresa{
-
     private $nome;
-    private $cnpj;
-    private $endereco;
+    private $cnpj; // implementar o get e set
+    private $endereco; // implementar o get e set
 
-    public function __construct($nome, $cnpj) {
+    public function __construct($nome, $cnpj)
+    {
         $this->nome = $nome;
         $this->cnpj = $cnpj;
     }
 
-    public function contratar(Funcionario $funcionario){
-
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
     }
 
-    public function demitir($id){
-
+    public function getNome()
+    {
+        return $this->nome;
     }
 
-    public function listarFuncionarios(){
+    public function contratar(Funcionario $funcionario)
+    {
+        $data = [
+            'id' => $funcionario->getId(),
+            'nome' => $funcionario->getNome(),
+            'idade' => $funcionario->getIdade(),
+            'cpf' => $funcionario->getCpf(),
+            'salario' => $funcionario->getSalario()
+        ];
 
+        $allData = readFileContent('files/funcionarios.txt');
+        array_push($allData, $data);
+        saveFileContent('files/funcionarios.txt', $allData);
+    }
+
+    public function demitir(string $id)
+    {
+        $allData = readFileContent('files/funcionarios.txt');
+
+        $filteredData =  array_values(array_filter($allData, function ($item) use ($id) {
+            return $item->id !== $id;
+        }));
+
+        saveFileContent('files/funcionarios.txt', $filteredData);
+    }
+
+    public function listarFuncionarios()
+    {
+        $data = readFileContent('files/funcionarios.txt');
+        return $data;
+    }
+
+    public function aumentarSalario(Funcionario $funcionario, $novaSalario)
+    {
+        // ........ IMPLEMENTACAO DO ALUNO..........
     }
 }
 
