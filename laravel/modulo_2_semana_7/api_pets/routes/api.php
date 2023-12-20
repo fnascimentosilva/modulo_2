da159fe0f72a3dd17807ce5a91370df3cd55ce4d
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-20 02:03:27 +0000

    #

--- laravel/modulo_2_semana_7/api_pets/routes/api.php
+++ laravel/modulo_2_semana_7/api_pets/routes/api.php
@@ -3,12 +3,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/pets', function(){
+Route::get('pets', function(){
     return 'Olá Laravel';
 });
 
 
 
-/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-}); */
+
