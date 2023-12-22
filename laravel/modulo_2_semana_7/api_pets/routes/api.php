10f410b992888c9628edf7c3c240ebf1c116a2df
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-22 22:58:41 +0000

    criando as rotas index e store

--- laravel/modulo_2_semana_7/api_pets/routes/api.php
+++ laravel/modulo_2_semana_7/api_pets/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PetController;
+use App\Http\Controllers\RaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('pets', [PetController::class, 'index']);
@@ -8,6 +9,10 @@ Route::post('pets', [PetController::class, 'store']);
 Route::delete('pets/{id}', [PetController::class, 'destroy']);
 Route::get('pets/{id}', [PetController::class, 'show']);
 Route::put('pets/{id}', [PetController::class, 'update']);
+
+
+Route::get('races', [RaceController::class, 'index']);
+Route::post('races', [RaceController::class, 'store']);
 
 
 
