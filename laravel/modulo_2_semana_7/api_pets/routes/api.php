de32aee1f8dc1acd8956e06cd11ded09e7a5fe14
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-22 03:27:06 +0000

    adicionando as rotas destroy, show e update

--- laravel/modulo_2_semana_7/api_pets/routes/api.php
+++ laravel/modulo_2_semana_7/api_pets/routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('pets', [PetController::class, 'index']);
 Route::post('pets', [PetController::class, 'store']);
-
+Route::delete('pets/{id}', [PetController::class, 'destroy']);
+Route::get('pets/{id}', [PetController::class, 'show']);
+Route::put('pets/{id}', [PetController::class, 'update']);
 
 
 
