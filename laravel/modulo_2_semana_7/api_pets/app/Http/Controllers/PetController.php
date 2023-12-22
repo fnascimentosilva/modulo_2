3b2a659d0cecf8c786a317502cb91463a7f49f84
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-22 03:24:56 +0000

    adicionando funcoes show, delete ,store e update

--- laravel/modulo_2_semana_7/api_pets/app/Http/Controllers/PetController.php
+++ laravel/modulo_2_semana_7/api_pets/app/Http/Controllers/PetController.php
@@ -2,19 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pet;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
     public function index()
     {
+        try {
+            $pets = Pet::all();
+            return $pets;
+        } catch (\Throwable $e) {
 
-        return 'Olá Laravel';
+            return;
+        }
     }
 
+
+    //pega o body
     public function store(Request $request)
     {
-//pega o body
-        var_dump($request->all());
+
+        $data = $request->all();
+
+        //Pegar somente um campo
+        /*  $name = $request->input('name'); */
+
+        $pet = Pet::create($data);
+
+        return $pet;
+    }
+
+    public function destroy($id)
+    {
+        $pet =  Pet::find($id);
+
+        if (!$pet)return $this->response('Pet nao encontrado', null, false, 404);
+
+        $pet->delete();
+
+        return $this->response(' ', null, true, 204);
+    }
+
+    public function show($id){
+
+        $pet = Pet::find($id);
+
+        if (!$pet)return $this->response('Pet nao encontrado', null, false, 404);
+
+        return $this->response(' ', $pet, true, 200);
+
+    }
+
+    public function update($id, Request $request){
+
+        $data = $request->all();
+
+        $pet =  Pet::find($id);
+
+        if (!$pet)return $this->response('Pet nao encontrado', null, false, 404);
+
+        $pet->update($data);
+
+        return $pet;
     }
 }
