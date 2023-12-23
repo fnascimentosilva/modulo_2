220e85d155907d8b5d001d979dc5855021dfbf0c
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-23 21:53:29 +0000

    adicionando a function store de Race

--- laravel/modulo_2_semana_9/api_pets_revisao/app/Http/Controllers/RaceController.php
+++ laravel/modulo_2_semana_9/api_pets_revisao/app/Http/Controllers/RaceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Race;
+use Exception;
 use Illuminate\Http\Request;
 
 class RaceController extends Controller
@@ -11,10 +12,31 @@ class RaceController extends Controller
     public function index()
     {
 
-            $races = Race::all();
-            return $races;
-
+        $races = Race::all();
+        return $races;
     }
 
-    
+
+    //Usado para fazer um cadastro atraves de um body
+    public function store(Request $request)
+    {
+
+        try {
+            $request->validate([
+                'name' => 'required|string|unique:races|max:50'
+
+            ]);
+
+            $data = $request->all();
+
+            //Pegar somente um campo
+            /*  $name = $request->input('name'); */
+
+            $race = Race::create($data);
+
+            return $race;
+        } catch (Exception $exception) {
+            return $this->response($exception->getMessage(), null, false, 400);
+        }
+    }
 }
