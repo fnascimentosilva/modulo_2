69870fdf2e1f3038ea69161db6d1ac0fe706531e
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-23 22:35:45 +0000

    add o httpresponse pra retornar o erro

--- laravel/modulo_2_semana_9/api_pets_revisao/app/Http/Controllers/RaceController.php
+++ laravel/modulo_2_semana_9/api_pets_revisao/app/Http/Controllers/RaceController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Race;
+use App\Traits\HttpResponses;
 use Exception;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class RaceController extends Controller
 {
+
+    use HttpResponses;
     //Lista todos ou parcialmente os dados de um recurso
     public function index()
     {
@@ -24,19 +29,15 @@ class RaceController extends Controller
         try {
             $request->validate([
                 'name' => 'required|string|unique:races|max:50'
-
             ]);
 
             $data = $request->all();
-
-            //Pegar somente um campo
-            /*  $name = $request->input('name'); */
 
             $race = Race::create($data);
 
             return $race;
         } catch (Exception $exception) {
-            return $this->response($exception->getMessage(), null, false, 400);
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
 }
