c8116503edbf1ba421d1ac5b8d1b8832450bdd92
fnascimentosilva <fabricionsilva26@gmail.com>
2023-12-24 00:20:31 +0000

    correcao de rota

--- laravel/modulo_2_semana_9/api_pets_revisao/routes/api.php
+++ laravel/modulo_2_semana_9/api_pets_revisao/routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\RaceController;
+use App\Http\Controllers\SpecieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('races', [RaceController::class, 'store']);
 Route::get('races', [RaceController::class, 'index']);
 
-Route::post('species', [RaceController::class, 'store']);
-Route::get('species', [RaceController::class, 'index']);
+Route::post('species', [SpecieController::class, 'store']);
+Route::get('species', [SpecieController::class, 'index']);
